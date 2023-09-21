0f92947e93e9e1d73aef8bd3723cae0c631d5696
AmstlerStephan <stephan.amstler@student.uibk.ac.at>
2023-09-21 19:44:59 +0200

    extract and parse haplotypes

--- bin/extract_haplotypes.py
+++ bin/extract_haplotypes.py
@@ -43,11 +43,74 @@ def parse_args(argv):
         help="input bam file to extract haplotypes from"
     )
     
-def extract_haplotypes(args):
+    args = parser.parse_args(argv)
+
+    return args
+
+
+def get_haplotypes(args):
     bam_file = args.BAM_FILE
     
-    samfile = pysam.AlignmentFile(bam_file, "rb")
-    iter = samfile.pileup()
+    query_names = get_query_names(bam_file)
+    haplotypes = extract_haplotypes(bam_file)
+    parsed_haplotypes = parse_haplotypes(haplotypes, query_names)
+    print(parsed_haplotypes)
+
+def get_query_names(bam_file):
+    query_names = dict()
+    with pysam.AlignmentFile(bam_file, "rb") as samfile:
+        for read in samfile.fetch():
+            query_names[read.query_name] = dict(haplotype = list(), qual = list())
+    print(query_names)
+    return query_names
+
+def extract_haplotypes(bam_file):
+    position_dict = dict()
+    with pysam.AlignmentFile(bam_file, "rb") as samfile:
+        # truncate = True truncates overlapping reads to positions which are aligned to the ref
+        # loop over columns of bam_file
+        for pileup_column in samfile.pileup(truncate = True):
+            # check for polymorphic position
+            if len(set(pileup_column.get_query_sequences(add_indels = True))) > 1:
+                pos = pileup_column.reference_pos
+                position_dict[pos] = list()
+            # loop over reads per column
+                for pileup_read in pileup_column.pileups:
+                    read = pileup_read.alignment
+                    read_pos = pileup_read.query_position
+                    if pileup_read.is_del:
+                        base = "D"
+                        qual = 70
+                    elif( pileup_read.indel > 0):
+                        base = read.query_sequence[read_pos:read_pos + pileup_read.indel]
+                        qual = read.query_qualities[read_pos:read_pos + pileup_read.indel]
+                    else:
+                        base = read.query_sequence[read_pos]
+                        qual = read.query_qualities[read_pos]
+                        
+                    
+                    read_info = dict(
+                        name = read.query_name, 
+                        base = base, 
+                        qual = qual)
+                    position_dict[pos].append(read_info)
+
+    return position_dict
+
+
+def parse_haplotypes(haplotypes, query_names):
+    for pos, reads in sorted(haplotypes.items()):
+        parsed_haplotypes = dict()
+
+        for read in reads:
+            name = read.get("name")
+            base = read.get("base")
+            qual = read.get("qual")
+            query_names[name]["haplotype"].append(base) 
+            query_names[name]["qual"].append(qual) 
+            
+    return query_names
+            
 
 def main(argv=sys.argv[1:]):
     """
@@ -65,7 +128,7 @@ def main(argv=sys.argv[1:]):
         raise ValueError("Invalid log level: %s" % args.log.upper())
     logging.basicConfig(level=numeric_level, format="%(message)s")
 
-    extract_haplotypes(args)
+    get_haplotypes(args)
 
 
 if __name__ == "__main__":
